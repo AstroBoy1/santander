a098192fbb22ce05b889f14cf1892378b4851699
omorim <omorim@hawaii.edu>
2019-03-13 08:40:00 +0000

    nn

--- nn.py
+++ nn.py
@@ -1,38 +1,122 @@
-from keras.models import Sequential
-from keras.layers import Dense
+# IMPORTS
 import pandas as pd
-from sklearn.metrics import roc_auc_score
 import numpy as np
-from sklearn import svm
-from collections import Counter
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.metrics import roc_auc_score
+from keras.models import Sequential, Model
+from keras.layers import Dense, Input, Flatten, Dropout, BatchNormalization, GaussianNoise
+from keras import callbacks
+import keras.backend as K
+# LOAD DATA
+df_train = pd.read_csv('data/train.csv', index_col=0)
+num_samples = 10000
+df_train = df_train[:num_samples]
+y_train = df_train.pop('target')
+len_train = len(df_train)
+df_test = pd.read_csv('data/test.csv', index_col=0)
+df_all = pd.concat((df_train, df_test), sort=False)
+prev_cols = df_all.columns
+
+# PREPROCESS
+scaler = StandardScaler()
+df_all[prev_cols] = scaler.fit_transform(df_all[prev_cols])
+df_train = df_all[0:len_train]
+df_test = df_all[len_train:]
+
+# CROSS VALIDATION
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=7)
+
+# LOGGER
+class Logger(callbacks.Callback):
+    def __init__(self, out_path='./', patience=10, lr_patience=3, out_fn='', log_fn=''):
+        self.auc = 0
+        self.path = out_path
+        self.fn = out_fn
+        self.patience = patience
+        self.lr_patience = lr_patience
+        self.no_improve = 0
+        self.no_improve_lr = 0
+
+    def on_train_begin(self, logs={}):
+        return
+
+    def on_train_end(self, logs={}):
+        return
+
+    def on_epoch_begin(self, epoch, logs={}):
+        return
+
+    def on_batch_begin(self, batch, logs={}):
+        return
+
+    def on_batch_end(self, batch, logs={}):
+        return
+
+    def on_epoch_end(self, epoch, logs={}):
+        cv_pred = self.model.predict(self.validation_data[0], batch_size=1024)
+        cv_true = self.validation_data[1]
+        auc_val = roc_auc_score(cv_true, cv_pred)
+        if self.auc < auc_val:
+            self.no_improve = 0
+            self.no_improve_lr = 0
+            print("Epoch %s - best AUC: %s" % (epoch, round(auc_val, 4)))
+            self.auc = auc_val
+            self.model.save(self.path + self.fn, overwrite=True)
+        else:
+            self.no_improve += 1
+            self.no_improve_lr += 1
+            print("Epoch %s - current AUC: %s" % (epoch, round(auc_val, 4)))
+            if self.no_improve >= self.patience:
+                self.model.stop_training = True
+            if self.no_improve_lr >= self.lr_patience:
+                lr = float(K.get_value(self.model.optimizer.lr))
+                K.set_value(self.model.optimizer.lr, 0.75*lr)
+                print("Setting lr to {}".format(0.75*lr))
+                self.no_improve_lr = 0
+
+        return
+
+# MODEL DEF
+def _Model():
+    inp = Input(shape=(200, 1))
+    d1 = Dense(16, activation='relu')(inp)
+    fl = Flatten()(d1)
+    preds = Dense(1, activation='sigmoid')(fl)
+    model = Model(inputs=inp, outputs=preds)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 
 
-if __name__ == "__main__":
+#RUN
+preds = []
+c = 0
+oof_preds = np.zeros((len(df_train), 1))
+for train, valid in cv.split(df_train, y_train):
+    print("VAL %s" % c)
+    X_train = np.reshape(df_train.iloc[train].values, (-1, 200, 1))
+    y_train_ = y_train.iloc[train].values
+    X_valid = np.reshape(df_train.iloc[valid].values, (-1, 200, 1))
+    y_valid = y_train.iloc[valid].values
+    model = _Model()
+    logger = Logger(patience=10, out_path='./', out_fn='cv_{}.h5'.format(c))
+    model.fit(X_train, y_train_, validation_data=(X_valid, y_valid), epochs=100, verbose=2, batch_size=256,
+              callbacks=[logger])
+    model.load_weights('cv_{}.h5'.format(c))
+    X_test = np.reshape(df_test.values, (200000, 200, 1))
+    curr_preds = model.predict(X_test, batch_size=2048)
+    oof_preds[valid] = model.predict(X_valid)
+    preds.append(curr_preds)
+    c += 1
+auc = roc_auc_score(y_train, oof_preds)
+print("CV_AUC: {}".format(auc))
 
-    length = 10000
-    train_data_df = pd.read_csv("data/train.csv")
-    train_data_x = train_data_df.drop(columns=["ID_code", "target"]).values
-    train_data_y = train_data_df["target"].values
-    valid_data_df = pd.read_csv("data/test.csv")
-    valid_data_x = valid_data_df.drop(columns=["ID_code"]).values
-
-    clf = svm.SVC()
-    print("Fitting data")
-    clf.fit(train_data_x[:length], train_data_y[:length])
-    print("Predicting data")
-    valid_pred = clf.predict(valid_data_x[:length])
-    print("Scoring data")
-    output_df = pd.DataFrame()
-    output_df["ID_code"] = valid_data_df["ID_code"]
-    output_df["target"] = valid_pred
-
-    #score = roc_auc_score(valid_data_y[:length], valid_pred[:length])
-    #print(score)
-
-    # model = Sequential()
-    # model.add(Dense(units=64, activation='relu', input_dim=100))
-    # model.add(Dense(units=10, activation='softmax'))
-    # model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    # model.fit(x_train, y_train, epochs=5, batch_size=32)
-    # loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-    # classes = model.predict(x_test, batch_size=128)
+# SAVE DATA
+preds = np.asarray(preds)
+preds = preds.reshape((5, 200000))
+preds_final = np.mean(preds.T, axis=1)
+#submission = pd.read_csv('./../input/sample_submission.csv')
+submission = pd.DataFrame()
+submission = pd.read_csv("data/sample_submission.csv")
+submission['target'] = preds_final
+submission.to_csv('submission20.csv', index=False)
