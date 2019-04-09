8af2175e0b0332382ca9ae64891cd8f82a80eb6f
omorim <omorim@hawaii.edu>
2019-04-09 09:56:17 +0000

    tuning

--- nn_bo.py
+++ nn_bo.py
@@ -17,9 +17,11 @@ from functools import partial
 from keras.optimizers import SGD
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedKFold
+import tensorflow as tf
+from keras.layers import Flatten
 
 class Logger(callbacks.Callback):
-    def __init__(self, out_path='./', patience=50, lr_patience=10, out_fn='', log_fn='', decay=0.5):
+    def __init__(self, out_path='./', patience=10, lr_patience=3, out_fn='', log_fn='', decay=0.5):
         self.auc = 0
         self.path = out_path
         self.fn = out_fn
@@ -84,15 +86,22 @@ def get_model(l1, drop1, l2, drop2):
         - model: specify the model just created so that we can later use it again.
     """
     model = Sequential()
-    model.add(Dense(int(l1), input_shape=(200,), name="l1"))
+    model.add(Dense(int(l1), input_shape=(200, 1), name="l1"))
     model.add(Activation('relu'))
     model.add(Dropout(drop1, name="drop1"))
     model.add(Dense(int(l2), name="l2"))
     model.add(Activation('relu'))
     model.add(Dropout(drop2, name="drop2"))
+    model.add(Flatten())
     model.add(Dense(1, name="l3"))
     model.add(Activation('sigmoid'))
     return model
+
+
+def auc(y_true, y_pred):
+    auc = tf.metrics.auc(y_true, y_pred)[1]
+    K.get_session().run(tf.local_variables_initializer())
+    return auc
 
 
 def fit_with(l1, drop1, l2, drop2, momentum, decay, batch_size, lr):
@@ -103,12 +112,12 @@ def fit_with(l1, drop1, l2, drop2, momentum, decay, batch_size, lr):
     # Train the model for a specified number of epochs.
     logger = Logger(patience=patience_epochs, lr_patience=lr_patience, out_path='./', out_fn='cv_{}.h5'.format(c))
     optimizer = SGD(lr=lr, decay=decay, momentum=momentum, nesterov=True)
-    model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer=optimizer)
+    model.compile(loss='binary_crossentropy', metrics=[auc],  optimizer=optimizer)
     best_scores = []
     for train, valid in cv.split(x, y):
-        x_train = x[train][:]
+        x_train = np.reshape(x[train][:], (-1, 200, 1))
         y_train = y[train][:]
-        x_valid = x[valid][:]
+        x_valid = np.reshape(x[valid][:], (-1, 200, 1))
         y_valid = y[valid][:]
     # Train the model with the train dataset.
         model.fit(x_train, y_train, epochs=1000, batch_size=int(batch_size), validation_data=[x_valid, y_valid], callbacks=[logger])
@@ -122,7 +131,10 @@ def fit_with(l1, drop1, l2, drop2, momentum, decay, batch_size, lr):
     # Return the accuracy.
     # best_score = max(logger.auc_list)
     print("AUCs: ", best_scores)
-    print("l1: %d drop1: %d l2: %d drop2: %d momentum: %d decay: %d batch_size: %d lr: %d" % (l1, drop1, l2, drop2, momentum, decay, batch_size, lr))
+    fh.write(str(best_scores))
+    fh.write("l1: %d drop1: %d l2: %d drop2: %d momentum: %d decay: %d batch_size: %d lr: %d" % (l1, drop1, l2, drop2, momentum, decay, batch_size, lr))
+    fh.write(str(np.mean(best_scores)))
+    fh.write("\n")
     return np.mean(best_scores)
 
 
@@ -137,13 +149,14 @@ if __name__ == '__main__':
     # Bounded region of parameter space
     patience_epochs = 10
     lr_patience = 3
-    pbounds = {'l1': (300, 400), 'drop1': (0, 0.5), 'l2': (50, 200), 'drop2': (0, 0.5), 'momentum': (0, 1), 'decay': (0.1, 0.75), 'batch_size': (1, 256), 'lr': (1e-4, 5e-2)}
+    fh = open("nn_logs.txt", "a")
+    pbounds = {'l1': (50, 100), 'drop1': (0, 0.5), 'l2': (10, 25), 'drop2': (0, 0.5), 'momentum': (0, 1), 'decay': (0.1, 0.75), 'batch_size': (1, 256), 'lr': (1e-3, 5e-2)}
     fit_with_partial = partial(fit_with)
     print("Creating optimizer")
     optimizer = BayesianOptimization(
         f=fit_with_partial,
         pbounds=pbounds,
-        verbose=2,  # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+        verbose=0,  # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
         random_state=1,
     )
     print("Optimizing")
@@ -153,3 +166,5 @@ if __name__ == '__main__':
         print("Iteration {}: \n\t{}".format(i, res))
 
     print(optimizer.max)
+    fh.write(optimizer.max)
+    fh.close()
